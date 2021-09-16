cb880da2b15c71be1a00ca8c745eeb32dffa9e60
jiangshuai321 <838318633@qq.com>
2021-09-16 18:11:22 +0800

    init

---
+++ app/src/main/java/com/xxx/sport02/TestUtils.java
@@ -0,0 +1,7 @@
+package com.xxx.sport02;
+
+public class TestUtils {
+    public static String getData(){
+        return "我是Sport02";
+    }
+}
---
+++ library/src/main/java/com/xxx/library/TestSportUtils.java
@@ -0,0 +1,7 @@
+package com.xxx.library;
+
+public class TestSportUtils {
+    public static String get(){
+        return "hahah";
+    }
+}
